1ed90c0467dcaf9f1e94a2d5794aa4e23a6f71ee
nicofishman <nicourifishman@gmail.com>
2024-02-05 13:42:08 -0300

    Remove TRPC ssr

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -3,22 +3,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import React, { useState } from 'react';
 
-import { trpc } from './client';
+import { trpcClient as client } from './client';
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
-    trpc.createClient({
+    client.createClient({
       links: [
         httpBatchLink({
-          url: 'http://localhost:3000/api/trpc',
+          url: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/api/trpc`,
         }),
       ],
     })
   );
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+    <client.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
+    </client.Provider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import { serverClient } from '@/app/_trpc/serverClient';
+'use client';
+
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { trpc } from '@/lib/trpc';
 import Link from 'next/link';
 
-export default async function Home() {
-  const { greeting } = await serverClient.hello({ text: 'Nico' });
-
+export default function Home() {
+  const { data, isLoading } = trpc.hello.useQuery({
+    text: 'world',
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
       <Button>Hola, soy un botón 👮</Button>
+      {isLoading ? (
+        <p className="text-white">Cargando saludo...</p>
+      ) : (
+        <p className="text-white">{data?.greeting}</p>
+      )}
       <Button variant="outline" asChild>
         <Link href={'/users'}>
           Soy un botón secundario que te lleva a la página de usuarios
