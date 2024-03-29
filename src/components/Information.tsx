d9df4897923a81ac88aeefbcf8e0529761553beb
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-28 21:29:21 -0300

    add: Componente Information

---
+++ src/components/Information.tsx
@@ -0,0 +1,14 @@
+
+
+
+const Information = () => {
+    return (
+        <div className="text-center">
+            <h1 className="font-bold font-bodoni text-[60px] text-black pt-[92px]">¿QUERÉS DESFILAR?</h1>
+            <p className="text-balance font-poppins pt-[42px]">En Expo Desfiles tenemos la oportunidad que estabas esperando. Vas a recibir entrenamientos y vas a poder desfilar <strong>¡GRATIS!</strong></p>
+            <h3 className="font-bodoni text-[40px] font-bold pt-[42px]">¿Te interesa?</h3>
+        </div>
+    );
+}
+
+export default Information;
