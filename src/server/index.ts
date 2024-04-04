b68835396104709cd893a8c2eb0d21e822221229
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-04-04 15:10:22 -0300

    delete: create modelo trpc

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,39 +4,7 @@ import { prisma } from '@/server/db';
 import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
-  perfil: router({
-    create: publicProcedure
-      .input(z.object({ nombre: z.string(), telefono: z.string() }))
-      .mutation(async ({ input }) => {
-        const telefonoSinSeparaciones = input.telefono
-          .replace(/\s+/g, '')
-          .replace(/\+/g, '');
 
-        const telefonoExistente = await prisma.perfil.findFirst({
-          where: {
-            telefono: telefonoSinSeparaciones,
-          },
-        });
-
-        if (telefonoExistente) {
-          throw new TRPCError({
-            code: 'CONFLICT',
-            message: 'El número de teléfono ya existe en el registro.',
-          });
-        }
-
-        const nombrePila = input.nombre.split(' ')[0];
-
-        await prisma.perfil.create({
-          data: {
-            nombreCompleto: input.nombre,
-            telefono: telefonoSinSeparaciones,
-            nombrePila: nombrePila,
-            etiquetas: { connect: { id: process.env.MODELO_ETIQUETA_ID } },
-          },
-        });
-      }),
-  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
