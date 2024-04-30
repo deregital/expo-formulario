6665c7c5fb63e6247efcfd19dc4e7e7690ba5a6e
nicofishman <nicourifishman@gmail.com>
2024-04-29 21:33:35 -0300

    feat: logo svg blanco

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,15 +1,10 @@
+import Logo from '@/components/Logo';
 import Image from 'next/image';
 
 const TopBar = () => {
   return (
     <div className="flex w-full items-center justify-center bg-topbar">
-      <Image
-        src={'/images/image.jpg'}
-        alt="ExpoDesfiles Logo"
-        className="py-3"
-        width={108}
-        height={54}
-      />
+      <Logo width={150} height={75} className="text-white" />
     </div>
   );
 };
