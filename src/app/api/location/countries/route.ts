77b267cac74adefc5c6bc9b5c2c201861b350c42
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2025-01-12 00:33:03 -0300

    fix: max date to input fields

--- src/app/api/location/countries/route.ts
+++ src/app/api/location/countries/route.ts
@@ -1,6 +1,6 @@
-import { getPassword, getUsername } from "@/server/actions";
-import { fetchClient } from "@/server/fetchClient";
-import { NextRequest, NextResponse } from "next/server";
+import { getPassword, getUsername } from '@/server/actions';
+import { fetchClient } from '@/server/fetchClient';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
@@ -11,29 +11,40 @@ export async function GET(req: NextRequest, res: NextResponse) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
-    const { data: dataLogin, error: errorLogin } = await fetchClient.POST('/auth/login', {
-      body: {
-        password: expo_manager_password,
-        username: expo_manager_username,
+    const { data: dataLogin, error: errorLogin } = await fetchClient.POST(
+      '/auth/login',
+      {
+        body: {
+          password: expo_manager_password,
+          username: expo_manager_username,
+        },
       }
-    })
+    );
 
     if (errorLogin) {
-      const messageString = errorLogin.message[0]
-      return NextResponse.json({ error: messageString }, { status: errorLogin.statusCode });
+      const messageString = errorLogin.message[0];
+      return NextResponse.json(
+        { error: messageString },
+        { status: errorLogin.statusCode }
+      );
     }
 
-    const { data: dataGetCountries } = await fetchClient.GET('/location/all-countries', {
-      headers: {
-        Authorization: `Bearer ${dataLogin?.backendTokens.accessToken}`,
-      },
-    })
+    const { data: dataGetCountries } = await fetchClient.GET(
+      '/location/all-countries',
+      {
+        headers: {
+          Authorization: `Bearer ${dataLogin?.backendTokens.accessToken}`,
+        },
+      }
+    );
 
     return NextResponse.json({
-      dataGetCountries: dataGetCountries?.countries
+      dataGetCountries: dataGetCountries?.countries,
     });
-
   } catch (error) {
-    return NextResponse.json({ error: 'Error al registrarse, por favor contactar con soporte' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Error al registrarse, por favor contactar con soporte' },
+      { status: 500 }
+    );
   }
 }
